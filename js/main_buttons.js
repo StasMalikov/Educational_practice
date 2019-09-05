4313157b201513a50779102f1c8bba9fa51dcc7b
stas_malikov <stas.malikov75@gmail.com>
2019-09-05 22:44:25 +0300

    начало работы

---
+++ js/main_buttons.js
@@ -0,0 +1,11 @@
+add_new.onclick = function() {
+
+};
+
+edit_exist.onclick = function() {
+
+};
+
+show_exist.onclick = function() {
+
+};
--- query.php
+++ query.php
@@ -1,4 +1,4 @@
-<?php // query.php
+<?php //fetchrow.php
   require_once 'login.php';
   $conn = new mysqli($hn, $un, $pw, $db);
   if ($conn->connect_error) die("Fatal Error");
@@ -11,16 +11,14 @@
 
   for ($j = 0 ; $j < $rows ; ++$j)
   {
-    $result->data_seek($j);
-    echo 'Author: '   . htmlspecialchars($result->fetch_assoc()['author'])   . '<br>';
-    $result->data_seek($j);
-    echo 'Title: '    . htmlspecialchars($result->fetch_assoc()['title'])    . '<br>';
-    $result->data_seek($j);
-    echo 'Category: ' . htmlspecialchars($result->fetch_assoc()['category']) . '<br>';
-    $result->data_seek($j);
-    echo 'Year: '     . htmlspecialchars($result->fetch_assoc()['year'])     . '<br>';
-    $result->data_seek($j);
-    echo 'ISBN: '     . htmlspecialchars($result->fetch_assoc()['isbn'])     . '<br><br>';
+
+    $row = $result->fetch_array(MYSQLI_ASSOC);
+
+    echo 'Author: '   . htmlspecialchars($row['author'])   . '<br>';
+    echo 'Title: '    . htmlspecialchars($row['title'])    . '<br>';
+    echo 'Category: ' . htmlspecialchars($row['category']) . '<br>';
+    echo 'Year: '     . htmlspecialchars($row['year'])     . '<br>';
+    echo 'ISBN: '     . htmlspecialchars($row['isbn'])     . '<br><br>';
   }
 
   $result->close();
